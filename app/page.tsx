99f7b5d0871abb1cfdf792b85dcb8918218eff63
p-mn19 <144673836+p-mn19@users.noreply.github.com>
2024-08-29 16:06:11 +0530

    commit

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,8 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-      <p className = "">
-        Hello Mayil!
+      <p className= 'dark-blue' font-bold>
+    Hello Mayil!
         </p>
     </div>
    
