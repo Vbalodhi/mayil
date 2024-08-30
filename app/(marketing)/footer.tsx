06fa9e4271ea55846852362d95bc18bdb816eb5b
p-mn19 <144673836+p-mn19@users.noreply.github.com>
2024-08-31 00:01:47 +0530

    added logo

---
+++ app/(marketing)/footer.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button"
+
+export const Footer = () => {
+    return(
+       <footer className="hidden lg:block h-20 w-full border-t-2 border-slate-200">
+          <div className="max-w-screen-lg mx-auto flex items-center justify-evenly h-full">
+            
+            </div>
+       </footer>
+    )
+}
