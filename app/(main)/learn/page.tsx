31f9dbc73230f22963ad0895d93b0cf51385ca14
Vbalodhi <144673308+Vbalodhi@users.noreply.github.com>
2024-08-31 17:49:31 +0530

    commit

--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -1,22 +1,9 @@
-import { Sidebar } from "lucide-react";
-
-type Props = {
-    children:React.ReactNode;
-};
-
-const MainLayout = ({
-    children,
-}: Props ) => {
+const LearnPage = () => {
     return (
-        <>
-        <Sidebar className="hidden lg:flex" / >
-        <main className="lg:pl-[256px] h-full pt-[50px] lg:pt-0">
-            <div className="bg-red-500 h-full">
-                {children}
-            </div>
-        </main>
-        </>
+        <div>
+            Learn Page
+        </div>
     );
-};
+ }
 
-export default MainLayout;  
+ export default LearnPage;
