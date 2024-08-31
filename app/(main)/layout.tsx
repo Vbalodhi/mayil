ab90c8f85550f8afbc8b479e4365f63cad5395fe
Vbalodhi <144673308+Vbalodhi@users.noreply.github.com>
2024-08-31 17:37:06 +0530

    commit

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,3 +1,5 @@
+import { Sidebar } from "lucide-react";
+
 type Props = {
     children:React.ReactNode;
 };
@@ -6,12 +8,15 @@ const MainLayout = ({
     children,
 }: Props ) => {
     return (
-    <>
-        <main className="pl-[256px] h-full">
+        <>
+        <Sidebar className="hidden lg:flex" / >
+        <main className="lg:pl-[256px] h-full pt-[50px] lg:pt-0">
             <div className="bg-red-500 h-full">
                 {children}
             </div>
         </main>
-    </>
+        </>
     );
 };
+
+export default MainLayout;  
