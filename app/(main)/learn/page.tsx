9215f5c94ddad43bd6ac83f9689a3abb169b8a61
Vbalodhi <144673308+Vbalodhi@users.noreply.github.com>
2024-08-31 17:34:58 +0530

    commit

--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -1,9 +1,22 @@
-const LearnPage = () => {
-    return (
-        <div>
-            Learn Page
-        </div>
-    );
- }
+import { Sidebar } from "lucide-react";
 
- export default LearnPage;
+type Props = {
+    children:React.ReactNode;
+};
+
+const MainLayout = ({
+    children,
+}: Props ) => {
+    return (
+        <>
+        <Sidebar className="hidden lg:flex" / >
+        <main className="lg:pl-[256px] h-full pt-[50px] lg:pt-0">
+            <div className="bg-red-500 h-full">
+                {children}
+            </div>
+        </main>
+        </>
+    );
+};
+
+export default MainLayout;  
